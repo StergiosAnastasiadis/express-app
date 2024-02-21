8bb575626d64cb6b6fe585928265a94006fd781b
StergiosAnastasiadis <Stelioslonx@outlook.com>
2024-02-21 21:06:51 +0200

    add type module to use ES6 import

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-const express = require('express')
-const { default: helmet } = require('helmet')
+// const express = require('express')
+// const { default: helmet } = require('helmet')
+import express from 'express'
+import helmet from 'helmet'
 
 const app = express()
 
