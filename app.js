b95995a2f982c22d104195f529ab357c4f7f12f8
StergiosAnastasiadis <Stelioslonx@outlook.com>
2024-03-01 15:51:37 +0200

    Change DB To PostgreSQL

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 import express from 'express'
 import helmet from 'helmet'
-import { connectDB } from './db/connect.js'
+import { db } from './db/connect.js'
 
 const app = express()
 
 app.use(helmet({ crossOriginEmbedderPolicy: false }))
 
-connectDB()
+// MongoDB
+// connectDB()
+
+// PostgreSQL
+db.connect()
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello Express!!!</h1>')
 })
 
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 8000
 app.listen(port, () => console.log(`Server listening on port: ${port}`))
--- db/connect.js
+++ db/connect.js
@@ -1,11 +1,23 @@
-import { connect } from "mongoose"
+// import { connect } from "mongoose"
 
-export async function connectDB() {
-    try {
-        const conn = await connect(process.env.MONGO_URI)
-        console.log(`MongoDB Connected: ${conn.connection.host}`)
-    } catch (error) {
-        console.error(`Error: ${error.message}`)
-        process.exit(1)
-    }
-}
+// Mongo DB
+// export async function connectDB() {
+//     try {
+//         const conn = await connect(process.env.MONGO_URI)
+//         console.log(`MongoDB Connected: ${conn.connection.host}`)
+//     } catch (error) {
+//         console.error(`Error: ${error.message}`)
+//         process.exit(1)
+//     }
+// }
+
+// PostgreSQL Database
+import pg from "pg"
+
+export const db = new pg.Client({
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_DATABASE,
+    password: process.env.POSTGRES_PASSWORD,
+    port: process.env.POSTGRES_PORT
+})
