b11653c35dc4b0c7b21ce9e07d6782460cd4328f
StergiosAnastasiadis <Stelioslonx@outlook.com>
2024-03-20 18:12:17 +0200

    Add Password encryption

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,5 +1,8 @@
 import { randomBytes } from 'crypto';
 import { db } from '../db/connect.js';
+// import { capitalize } from 'lodash';
+import _ from 'lodash';
+import bcrypt from 'bcryptjs'
 
 const authUser = async (req, res) => {
     const { email, password } = req.body
@@ -7,10 +10,23 @@ const authUser = async (req, res) => {
 }
 
 const registerUser = async (req, res) => {
-    const { email, password, firstname, lastname } = req.body
+    const email = req.body.email.toLowerCase().trim()
+    const password = req.body.email.trim()
+    const firstname = _.capitalize(req.body.firstname)
+    const lastname = _.capitalize(req.body.lastname)
+
+    if (!email || !password || !firstname || !lastname) {
+        res.status(400).json({ error: true, message: 'All Fields Required required - Email | Password  | Firstname | Lastname ' })
+    }
+
     const activationToken = randomBytes(20).toString('hex');
 
-    await db.query('INSERT INTO users (email, password, firstname, lastname, active, "activationToken") VALUES ($1, $2, $3, $4, $5, $6)', [email.trim().toLowerCase(), password, firstname, lastname, false, activationToken])
+    const encryptedPassword = bcrypt.hashSync(password, 10)
+    console.log(encryptedPassword);
+
+    await db.query('INSERT INTO users (email, password, firstname, lastname, active, "activationToken") VALUES ($1, $2, $3, $4, $5, $6)',
+        [email, encryptedPassword, firstname, lastname, false, activationToken]
+    )
 
     res.json({ email, password, firstname, lastname })
 }
