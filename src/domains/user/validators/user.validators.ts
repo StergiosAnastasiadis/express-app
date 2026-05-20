a802f90b93debef960989bf979c3e124869ae706
StergiosAnastasiadis <Stelioslonx@outlook.com>
2026-05-20 14:53:13 +0300

    Refactor the "user.validator.ts" file.

--- src/domains/user/validators/user.validators.ts
+++ src/domains/user/validators/user.validators.ts
@@ -4,20 +4,24 @@ import * as z from 'zod';
 const email = z.string().transform((e) => e.toLowerCase().trim());
 const password = z.string().transform((p) => p.trim());
 
+const firstname = z
+  .string()
+  .min(2)
+  .transform((f) => _.capitalize(f.trim()));
+
+const lastname = z
+  .string()
+  .min(2)
+  .transform((l) => _.capitalize(l.trim()));
+
 export const UserAuthSchema = z.strictObject({
   email,
   password,
 });
 
 export const UserRegisterSchema = z.strictObject({
-  email: z.email(),
-  firstname: z
-    .string()
-    .min(2)
-    .transform((f) => _.capitalize(f.trim())),
-  lastname: z
-    .string()
-    .min(2)
-    .transform((l) => _.capitalize(l.trim())),
+  email,
+  firstname,
+  lastname,
   password,
 });
