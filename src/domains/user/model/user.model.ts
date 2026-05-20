47b9b98cd6ab9e2294463edf3f4f2f428fafd89e
StergiosAnastasiadis <Stelioslonx@outlook.com>
2026-05-20 15:40:33 +0300

    Create the "AuthUser" and "RegisterUser" Models.

--- src/domains/user/model/user.model.ts
+++ src/domains/user/model/user.model.ts
@@ -1,3 +1,6 @@
+export type AuthUser = Pick<User, 'email' | 'password'>;
+export type RegisterUser = Pick<User, 'email' | 'firstname' | 'lastname' | 'password'>;
+
 export interface User {
   activationToken: string;
   active: boolean;
