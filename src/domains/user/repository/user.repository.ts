3cc60354d0eebadde9e21903299b73f83c191b3a
StergiosAnastasiadis <Stelioslonx@outlook.com>
2026-05-21 10:58:10 +0300

    Add the users id inside the "getUser" Select query.

--- src/domains/user/repository/user.repository.ts
+++ src/domains/user/repository/user.repository.ts
@@ -5,7 +5,7 @@ import type { User } from '../model/user.model.js';
 import { db } from '../../../db/connect.js';
 
 export const getUser = async (email: string) => {
-  const query = `SELECT email, password, firstname, lastname FROM users WHERE email='${email}'`;
+  const query = `SELECT id, email, password, firstname, lastname FROM users WHERE email='${email}'`;
   const user: QueryResult<User> = await db.query(query);
 
   if (user.rows.length > 0) return user.rows[0];
