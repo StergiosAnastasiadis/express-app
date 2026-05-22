0611a94e5eff64b3786fe4f9ab61365f8878a29d
StergiosAnastasiadis <Stelioslonx@outlook.com>
2026-05-22 15:42:19 +0300

    Add the "DEFAULT FALSE" into the "active" Field in the DB.

--- src/db/CreateTables/CREATE_USERS_TABLE.sql
+++ src/db/CreateTables/CREATE_USERS_TABLE.sql
@@ -4,7 +4,7 @@ CREATE TABLE users (
 	password TEXT NOT NULL,
 	firstname VARCHAR(45) NOT NULL,
 	lastname VARCHAR(45) NOT NULL,
-	active boolean NOT NULL,
+	active boolean NOT NULL DEFAULT FALSE,
 	activation_token VARCHAR(40) NOT NULL,
 	created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 )
