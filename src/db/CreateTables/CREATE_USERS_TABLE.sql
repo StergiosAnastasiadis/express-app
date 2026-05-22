444a71be000e5c7a94c459014e809ea0a1eff5bc
StergiosAnastasiadis <Stelioslonx@outlook.com>
2026-05-22 15:39:30 +0300

    Add the created_at field on the users table.

--- src/db/CreateTables/CREATE_USERS_TABLE.sql
+++ src/db/CreateTables/CREATE_USERS_TABLE.sql
@@ -5,5 +5,6 @@ CREATE TABLE users (
 	firstname VARCHAR(45) NOT NULL,
 	lastname VARCHAR(45) NOT NULL,
 	active boolean NOT NULL,
-	activation_token VARCHAR(40) NOT NULL 
+	activation_token VARCHAR(40) NOT NULL,
+	created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 )
